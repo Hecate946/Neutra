710d286680272471205fa5c98c3bb45ad9ba9234
Hecatex <74381783+Hecatex@users.noreply.github.com>
2021-08-03 23:04:34 -0700

    sql table update

--- data/scripts/config.sql
+++ data/scripts/config.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS config (
     client_id BIGINT PRIMARY KEY,
-    avatar_saver_webhook_id BIGINT,
-    avatar_saver_webhook_token TEXT,
     presence TEXT,
     activity TEXT,
     status TEXT,
-    version REAL DEFAULT 5, --Implemented automatic verions in 5.0
+    version REAL DEFAULT 1,
     ownerlocked BOOLEAN DEFAULT False,
     reboot_invoker TEXT,
     reboot_message_id BIGINT,
