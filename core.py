b7081fee8ad02ceb7bc3b588d0dd94c1fc11b7c3
FateWalker <opabravo@gmail.com>
2021-08-22 18:39:46 +0800

    Update core.py
    Fix typo

--- core.py
+++ core.py
@@ -229,7 +229,7 @@ class Neutra(commands.AutoShardedBot):
         self.dispatch("unloaded_extension", name)
         return super().load_extension(name, package=package)
 
-    def load_extension(self, name, *, package=None):
+    def reload_extension(self, name, *, package=None):
         self.dispatch("reloaded_extension", name)
         return super().load_extension(name, package=package)
 
